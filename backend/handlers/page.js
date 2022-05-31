27053a2e8d5d5c0044d3237992f6fdbfae9f781c
fun <lljxx1@gmail.com>
2022-05-31 14:17:18 +0800

    add /detect

--- backend/handlers/page.js
+++ backend/handlers/page.js
@@ -7,70 +7,75 @@ async function detectUrl(req, res) {
   const link = req.query.link;
   const forceDetect = req.query.forceDetect;
   const parsed = getTopDomain(link);
+  try {
+    const domainStat = await DomainSummary.findOne({
+      where: {
+        host: parsed.host,
+      },
+    });
 
-  const domainStat = await DomainSummary.findOne({
-    where: {
-      host: parsed.host,
-    },
-  });
+    const needRefresh = forceDetect
+      ? true
+      : domainStat
+      ? Date.now() - domainStat.lastDetect > 60 * 1000 * 60 * 3
+      : true;
 
-  const needRefresh = forceDetect
-    ? true
-    : domainStat
-    ? Date.now() - domainStat.lastDetect > 60 * 1000 * 60 * 3
-    : true;
+    await increaseCount("total");
 
-  await increaseCount("total");
+    if (!needRefresh) {
+      return {
+        isBlack: domainStat.isBlack,
+      };
+    }
 
-  if (!needRefresh) {
-    return {
-      isBlack: domainStat.isBlack,
+    const { data } = await axios.get(process.env.DETECTOR_ENDPOINT, {
+      params: {
+        link: link,
+      },
+    });
+
+    const detectResult = {
+      isBlack: data.uniqueActions.length > 0,
+      lastDetect: Date.now(),
     };
+
+    await DetectHistory.create({
+      link,
+      host: parsed.host,
+      detail: JSON.stringify(data),
+    });
+
+    if (domainStat) {
+      domainStat.count = domainStat.count + 1;
+      await domainStat.update(
+        Object.assign(
+          {
+            count: domainStat.count + 1,
+          },
+          detectResult
+        )
+      );
+    } else {
+      await DomainSummary.create(
+        Object.assign(
+          {
+            host: parsed.host,
+            topDomain: parsed.topDomain,
+            count: 1,
+          },
+          detectResult
+        )
+      );
+    }
+
+    res.json({
+      isBlack: detectResult.isBlack,
+    });
+  } catch (e) {
+    res.json({
+        error: e.toString()
+    }); 
   }
-
-  const { data } = await axios.get(process.env.DETECTOR_ENDPOINT, {
-    params: {
-      link: link,
-    },
-  });
-
-  const detectResult = {
-    isBlack: data.uniqueActions.length > 0,
-    lastDetect: Date.now(),
-  };
-
-  await DetectHistory.create({
-    link,
-    host: parsed.host,
-    detail: JSON.stringify(data),
-  });
-
-  if (domainStat) {
-    domainStat.count = domainStat.count + 1;
-    await domainStat.update(
-      Object.assign(
-        {
-          count: domainStat.count + 1,
-        },
-        detectResult
-      )
-    );
-  } else {
-    await DomainSummary.create(
-      Object.assign(
-        {
-          host: parsed.host,
-          topDomain: parsed.topDomain,
-          count: 1,
-        },
-        detectResult
-      )
-    );
-  }
-
-  res.json({
-    isBlack: detectResult.isBlack,
-  });
 }
 module.exports = {
   detectUrl,
