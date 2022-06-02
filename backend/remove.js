9e24c9f4a37e3f1dd36587d8934499df30414813
fun <lljxx1@gmail.com>
2022-06-03 00:19:51 +0800

    exclude whitelist

---
+++ backend/remove.js
@@ -0,0 +1,11 @@
+const { DomainSummary } = require("./schema");
+const { Op } = require('sequelize')
+// init();
+
+DomainSummary.destroy({
+  where: {
+    topDomain: {
+      [Op.in]: ["premint.xyz", 'synthetix.io', "fractal.is", "nexustracker.io"],
+    },
+  },
+});
