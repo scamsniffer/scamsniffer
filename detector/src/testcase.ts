4a7c48b4fc62dedf99df429866a74c10b4872d05
fun <lljxx1@gmail.com>
2022-06-08 20:13:28 +0800

    ver:0.0.6

--- detector/src/testcase.ts
+++ detector/src/testcase.ts
@@ -1,54 +1,54 @@
-import { Detector } from "./detector";
-import { TEST_TWEETS } from "./__fixtures__/testTweets";
-import { BLACK_LIST } from "./__fixtures__/blackList";
+// import { Detector } from "./detector";
+// import { TEST_TWEETS } from "./__fixtures__/testTweets";
+// import { BLACK_LIST } from "./__fixtures__/blackList";
 
-async function testWhite() {
-    const detector = new Detector({});
-     const listScam = [];
-     for (let index = 0; index < TEST_TWEETS.length; index++) {
-        const startTime = Date.now();
-       const result = await detector.detectScam(TEST_TWEETS[index]);
-       const spend = Date.now() - startTime;
-       if (result) {
-         console.error("shouldWhite", TEST_TWEETS[index], result);
-         listScam.push(result);
-       } else {
+// async function testWhite() {
+//     const detector = new Detector({});
+//      const listScam = [];
+//      for (let index = 0; index < TEST_TWEETS.length; index++) {
+//         const startTime = Date.now();
+//        const result = await detector.detectScam(TEST_TWEETS[index]);
+//        const spend = Date.now() - startTime;
+//        if (result) {
+//          console.error("shouldWhite", TEST_TWEETS[index], result);
+//          listScam.push(result);
+//        } else {
 
-       }
-       console.log("spend", spend, index, 'isWhite', result === null);
-     }
+//        }
+//        console.log("spend", spend, index, 'isWhite', result === null);
+//      }
 
-     console.log(TEST_TWEETS.length, listScam.length, listScam);
-}
-
-
-async function testBlack() {
-    const detector = new Detector({});
-   const listScam = [];
-   for (let index = 0; index < BLACK_LIST.length; index++) {
-       const startTime = Date.now();
-     const result = await detector.detectScam(BLACK_LIST[index]);
-      const spend = Date.now() - startTime;
-     if (result) {
-       listScam.push(result);
-     } else {
-       console.error('shouldBlack', BLACK_LIST[index], result);
-     }
-     console.log(
-       "spend",
-       spend,
-       index,
-       "isBlack",
-       result !== null
-     );
-   }
-   console.log(listScam.length, BLACK_LIST.length);
-}
+//      console.log(TEST_TWEETS.length, listScam.length, listScam);
+// }
 
 
-async function test() {
-    await testWhite();
-    await testBlack();
-}
+// async function testBlack() {
+//     const detector = new Detector({});
+//    const listScam = [];
+//    for (let index = 0; index < BLACK_LIST.length; index++) {
+//        const startTime = Date.now();
+//      const result = await detector.detectScam(BLACK_LIST[index]);
+//       const spend = Date.now() - startTime;
+//      if (result) {
+//        listScam.push(result);
+//      } else {
+//        console.error('shouldBlack', BLACK_LIST[index], result);
+//      }
+//      console.log(
+//        "spend",
+//        spend,
+//        index,
+//        "isBlack",
+//        result !== null
+//      );
+//    }
+//    console.log(listScam.length, BLACK_LIST.length);
+// }
 
-test();
+
+// async function test() {
+//     await testWhite();
+//     await testBlack();
+// }
+
+// test();
