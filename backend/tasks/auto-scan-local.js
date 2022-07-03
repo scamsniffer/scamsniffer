d99ef92440d61bd3d03842c7e6988c03feb7c9ee
fun <lljxx1@gmail.com>
2022-07-03 23:20:35 +0800

    add

--- backend/tasks/auto-scan-local.js
+++ backend/tasks/auto-scan-local.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const API = `https://api.scamsniffer.io/detect`;
 require("dotenv").config();
 
-
 async function detectDomain(link) {
   console.log("process.env.DETECTOR_ENDPOINT", process.env.DETECTOR_ENDPOINT, link);
   const { data: pageData } = await axios.get("http://localhost:8080/detect", {
@@ -14,7 +13,7 @@ async function detectDomain(link) {
 
   console.log("pageData", link, pageData.hackerAddress);
   const { data } = await axios.post(
-    API + "?forceDetect=1&url=" + encodeURIComponent(link),
+    API + "?forceDetect=1&link=" + encodeURIComponent(link),
     {
       token: process.env.AUTH_TOKEN,
       data: pageData,
@@ -31,17 +30,33 @@ async function detectRecentDomain() {
   for (let index = 0; index < recentDomains.length; index++) {
     const recentDomain = recentDomains[index];
     try {
-      // const recentScams = await ScamList.findAll({
-      //   limit: 1,
-      //   where: {
-      //     host: recentDomain.host,
-      //   },
-      // });
-      // if (recentScams.length) {
-      //   console.log("detect", recentDomain.host);
-      //   await detectDomain(recentScams[0].link);
-      // } else {
-      await detectDomain(`https://${recentDomain.host}`);
+      const { data: lastDates } = await axios.get(
+        "https://api.scamsniffer.io/detectHistory?host=" +
+          recentDomain.host +
+          "&fields=createdAt&sort=-id"
+      );
+
+      const lastDate = lastDates.length ? lastDates[0].createdAt : null;
+      if (lastDate) {
+        const timeLeft = new Date(lastDate).getTime() - Date.now();
+        if (timeLeft < 60 * 1000 * 60) {
+          console.log('too short')
+          // continue;
+        }
+      }
+
+
+        // const recentScams = await ScamList.findAll({
+        //   limit: 1,
+        //   where: {
+        //     host: recentDomain.host,
+        //   },
+        // });
+        // if (recentScams.length) {
+        //   console.log("detect", recentDomain.host);
+        //   await detectDomain(recentScams[0].link);
+        // } else {
+        await detectDomain(`https://${recentDomain.host}`);
       // }
     } catch (e) {
       console.log("error", e);
--- backend/tasks/auto-scan.js
+++ backend/tasks/auto-scan.js
@@ -13,7 +13,7 @@ async function detectDomain(link) {
 
 async function detectRecentDomain() {
   const recentDomains = await DomainSummary.findAll({
-    limit: 50,
+    limit: 15,
     needReport: 1,
     order: [
         ['id', 'desc']
