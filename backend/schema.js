0d73633b3d4aa3fc0b46f5cadecd7724306bd502
fun <lljxx1@gmail.com>
2022-07-12 13:17:47 +0800

    add

--- backend/schema.js
+++ backend/schema.js
@@ -246,15 +246,17 @@ const Cache = sequelize.define(
 );
 
 async function init() {
-  await ScamList.sync({ alter: true });
-  await Cache.sync({ alter: true });
-  await DomainSummary.sync({ alter: true });
-  await Summary.sync({ alter: true });
-  await TwitterSummary.sync({ alter: true });
-  await Project.sync({ alter: true });
-  await DetectHistory.sync({ alter: true });
+  // await ScamList.sync({ alter: true });
+  // await Cache.sync({ alter: true });
+  // await DomainSummary.sync({ alter: true });
+  // await Summary.sync({ alter: true });
+  // await TwitterSummary.sync({ alter: true });
+  // await Project.sync({ alter: true });
+  // await DetectHistory.sync({ alter: true });
+
+  const res = await sequelize.query("DROP TABLE detect_histories_backup");
+  console.log(res);
   await ScamActivity.sync({ alter: true });
-  
 }
 
 if (isFirst) {
