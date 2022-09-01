7c6589814a78eb6c4bd3bada272d0fcb830ffbc0
fun <lljxx1@gmail.com>
2022-09-01 11:48:26 +0800

    add

--- backend/tasks/auto-report.js
+++ backend/tasks/auto-report.js
@@ -3,35 +3,39 @@ const { reportScam } = require("../reporter");
 const { increaseCount } = require('../utils/summary');
 
 async function getAndReport() {
-  const needReportDomains = await DomainSummary.findAll({
-    limit: 10,
-    where: {
-      needReport: 1,
-      reported: 0,
-    },
-  });
-
-  let reported = 0
-  for (let index = 0; index < needReportDomains.length; index++) {
-    const needReportDomain = needReportDomains[index];
-    const domainUrls = await ScamList.findAll({
-      raw: true,
-      limit: 5,
+  try {
+    const needReportDomains = await DomainSummary.findAll({
+      limit: 10,
       where: {
-        host: needReportDomain.host,
+        needReport: 1,
+        reported: 0,
       },
     });
-    if (domainUrls.length) {
-      const reportUrl = domainUrls[0].link;
-      await reportScam(reportUrl);
-      needReportDomain.reported = 1
-      await needReportDomain.save()
-      reported++
+  
+    let reported = 0
+    for (let index = 0; index < needReportDomains.length; index++) {
+      const needReportDomain = needReportDomains[index];
+      const domainUrls = await ScamList.findAll({
+        raw: true,
+        limit: 5,
+        where: {
+          host: needReportDomain.host,
+        },
+      });
+      if (domainUrls.length) {
+        const reportUrl = domainUrls[0].link;
+        await reportScam(reportUrl);
+        needReportDomain.reported = 1
+        await needReportDomain.save()
+        reported++
+      }
     }
+  
+    if (reported) await increaseCount("reported", reported);
+    console.log("found", needReportDomains.length);
+  } catch(e) {
+    
   }
-
-  if (reported) await increaseCount("reported", reported);
-  console.log("found", needReportDomains.length);
   setTimeout(getAndReport, 10 * 1000);
 }
 
